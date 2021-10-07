fbb04071b72c709d2d15c618ab7f8376e58dc701
Anton Bardin <a2bardin@gmail.com>
2021-10-07 13:12:11 +0300

    Eeg -> eeg.py

---
+++ code/eeg.py
@@ -0,0 +1,41 @@
+import numpy as np
+from brainflow import BoardShim
+
+class Eeg:
+    def __init__(self, board_shim):
+        self.board_id = board_shim.get_board_id()
+        self.board_shim = board_shim
+        self.num_channel = BoardShim.get_package_num_channel(self.board_id)
+        self.exg_channels = BoardShim.get_exg_channels(self.board_id)
+        self.sample_rate = BoardShim.get_sampling_rate(self.board_id)
+
+        # create buffer
+        self.BUFFER_SIZE = self.sample_rate
+        self.buff = np.zeros((self.BUFFER_SIZE, len(self.exg_channels)))
+
+        # print(f'{BoardShim.get_board_descr(self.board_id)}\n')
+        # print(f'num channel = {self.num_channel}')
+        # print(self.exg_channels, end='\n\n')
+
+        # Filling the buffer
+        self.buffer_fill()
+
+    def buffer_fill(self):
+        i = 0
+        while i < self.BUFFER_SIZE:
+            data = self.board_shim.get_board_data(1)
+            if np.any(data):
+                # current_num = data[self.num_channel]
+                self.current_exg = data[self.exg_channels]
+                self.buff[i, :] = self.current_exg[:, 0]
+                i += 1
+
+    def capture(self):
+        i = 0
+        while i < 500:
+            data = self.board_shim.get_board_data(1)
+            if np.any(data):
+                self.current_exg = data[self.exg_channels]
+                self.buff = np.roll(self.buff, -1, axis=0)
+                self.buff[-1] = self.current_exg[:, 0]
+                i += 1
--- code/test.py
+++ code/test.py
@@ -269,34 +269,78 @@
 #     main_win.show()
 #     sys.exit(app.exec())
 
-from PySide6.QtWidgets import QVBoxLayout, QLabel, QPushButton, QWidget, QMainWindow, QApplication
-from PySide6.QtCore import QTimer
-
 import sys
 import time
+import traceback
 
-from PySide6.QtCore import QRunnable, Slot, QThreadPool
+from PySide6.QtCore import (QObject, QRunnable, QThreadPool, QTimer, Signal,
+                            Slot)
+from PySide6.QtWidgets import (QApplication, QLabel, QMainWindow, QPushButton,
+                               QVBoxLayout, QWidget)
+
+
+class WorkerSignals(QObject):
+    '''
+    Defines the signals available from a running worker thread.
+    Supported signals are:
+    finished
+        No data
+    error
+        tuple (exctype, value, traceback.format_exc() )
+    result
+        object data returned from processing, anything
+    '''
+    finished = Signal()  # QtCore.Signal
+    error = Signal(tuple)
+    result = Signal(object)
+    progress = Signal(int)
+
 
 class Worker(QRunnable):
     '''
     Worker thread
+    Inherits from QRunnable to handler worker thread setup, signals and wrap-up.
+    :param callback: The function callback to run on this worker thread. Supplied args and
+                     kwargs will be passed through to the runner.
+    :type callback: function
+    :param args: Arguments to pass to the callback function
+    :param kwargs: Keywords to pass to the callback function
     '''
+    def __init__(self, fn, *args, **kwargs):
+        super(Worker, self).__init__()
+        self.fn = fn
+        self.args = args
+        self.kwargs = kwargs
+        self.signals = WorkerSignals()
+
+        self.kwargs['progress_callback'] = self.signals.progress
 
     @Slot()  # QtCore.Slot
     def run(self):
         '''
-        Your code goes in this function
+        Initialise the runner function with passed args, kwargs.
         '''
-        print("Thread start")
-        time.sleep(5)
-        print("Thread complete")
+        # Retrieve args/kwargs here; and fire processing using them
+        try:
+            result = self.fn(*self.args, **self.kwargs)
+        except:
+            traceback.print_exc()
+            extype, value = sys.exc_info()[:2]
+            self.signals.error.emit((extype, value, traceback.format_exc()))
+        else:
+            # Return the result of the processing
+            self.signals.result.emit(result)
+        finally:
+            self.signals.finished.emit()  # Done
+
 
 class MainWindow(QMainWindow):
     def __init__(self):
         super(MainWindow, self).__init__()
 
         self.threadpool = QThreadPool()
-        print("Multithreading with maximum %d threads" % self.threadpool.maxThreadCount())
+        print("Multithreading with maximum %d threads" %
+              self.threadpool.maxThreadCount())
 
         self.counter = 0
 
@@ -321,8 +365,31 @@ class MainWindow(QMainWindow):
         self.timer.timeout.connect(self.recurring_timer)
         self.timer.start()
 
+    def progress_fn(self, n):
+        print("%d%%" % n)
+
+    def execute_this_fn(self, progress_callback):
+        for n in range(5):
+            time.sleep(1)
+            progress_callback.emit(n * 100 / 4)
+
+        return "Done"
+
+    def print_output(self, s):
+        print(s)
+
+    def thread_complete(self):
+        print("THREAD COMPLETE!")
+
     def oh_no(self):
-        worker = Worker()
+        # Pass the function to exetute
+        # Any other args, kwargs are passed to the run function
+        worker = Worker(self.execute_this_fn)
+        worker.signals.result.connect(self.print_output)
+        worker.signals.finished.connect(self.thread_complete)
+        worker.signals.progress.connect(self.progress_fn)
+
+        # Execute
         self.threadpool.start(worker)
 
     def recurring_timer(self):
@@ -332,4 +399,4 @@ class MainWindow(QMainWindow):
 
 app = QApplication(sys.argv)
 window = MainWindow()
-app.exec_()
+app.exec()
