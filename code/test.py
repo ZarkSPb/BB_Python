db6790a59b7ed0894c01210e19bbb32774120641
Anton Bardin <a2bardin@gmail.com>
2021-11-11 16:15:26 +0300

    edits

--- code/test.py
+++ code/test.py
@@ -7,13 +7,19 @@ f = pyedflib.EdfReader(file_name)
 n = f.signals_in_file
 signal_labels = f.getSignalLabels()
 
-print(f.getPatientName())
-print(f.getPatientCode())
-print(f.getBirthdate())
-print(f.getStartdatetime())
+print(f.getHeader())
 
+# print(f.getPatientName())
+# print(f.getPatientCode())
+# print(f.getBirthdate())
+# print(f.getStartdatetime())
+
+sample_freqs = f.getSampleFrequencies()
 
 for s in signal_labels:
     print(s)
+
+for sample_freq in sample_freqs:
+    print(sample_freq)
 
 f.close()
