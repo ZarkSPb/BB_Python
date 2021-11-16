5c743acbac3e1a4faee4e22ecef5a127c081ea20
Anton Bardin <a2bardin@gmail.com>
2021-11-16 13:29:51 +0300

    some edits

--- code/chart_analize.py
+++ code/chart_analize.py
@@ -20,28 +20,35 @@ class ChartAn(QChartView):
 
         # /////////////////////////////////////////////////////////////// axis_c
         axis_c = QCategoryAxis()
-        axis_c.setRange(0, self.num_ch)
+        axis_c.setRange(0, self.num_ch * 100)
         axis_c.setGridLineVisible(False)
         axis_c.setLabelsPosition(QCategoryAxis.AxisLabelsPositionOnValue)
         axis_c.setTruncateLabels(False)
-        self.update_channels_axis(axis_c, session, (100, 100, 100, 100), self.num_ch)
+        self.update_channels_axis(axis_c, self.ch_names, (100, 100, 100, 100))
         chart.addAxis(axis_c, QtCore.Qt.AlignLeft)
 
         super(ChartAn, self).__init__(chart)
 
-    # ///////////////////////////////////////////////////////// Update CHANNELS axis
-    def update_channels_axis(axis_c, session, amps, num_ch):
+    # ///////////////////////////////////////////////////// Update CHANNELS axis
+    def update_channels_axis(self, axis_c, ch_names, amps):
+        num_ch = len(ch_names)
+        
         labels = axis_c.categoriesLabels()
         for i in range(len(labels)):
             axis_c.remove(labels[i])
 
-        axis_c.append(f'{-amps[0]}', 0)
-        channel_names = session.get_eeg_ch_names()
-        for i, ch_name in enumerate(channel_names[num_ch - 1::-1]):
-            axis_c.append(f'{-amps[i] // 2}' + i * ' ', i + 0.25)
-            axis_c.append(f'--{ch_name}--', i + 0.5)
-            axis_c.append(f'{amps // 2}' + i * ' ', i + 0.75)
-            if i < num_ch - 1:
-                axis_c.append(f'({amps})' + i * ' ', i + 1)
-            else:
-                axis_c.append(f'{amps}', i + 1)
+        for i, ch_names in enumerate(ch_names[num_ch - 1::-1]):
+            interval = amps[i] // 5
+            for j in range(interval, amps[i], interval):
+                axis_c.append(str(j), amps[i] // (j * interval))
+                print(j)
+
+
+
+            # axis_c.append(f'{-amps[i] // 2}' + i * ' ', i + 0.25)
+            # axis_c.append(f'--{ch_name}--', i + 0.5)
+            # axis_c.append(f'{amps // 2}' + i * ' ', i + 0.75)
+            # if i < num_ch - 1:
+            #     axis_c.append(f'({amps})' + i * ' ', i + 1)
+            # else:
+            #     axis_c.append(f'{amps}', i + 1)
